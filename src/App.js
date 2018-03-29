da47f866011c6babbee2c93ca2f5ad849627bb00
jessekrubin <jessekrubin@gmail.com>
2018-03-29 15:24:18 -0400

    got it working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Link, Switch, Route } from 'react-router-dom';
+
+
+import {Navbar, NavItem} from 'react-materialize';
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>(The) Jesse Rubin Dot Com</h1>
+         </div>
+      );
+   }
+}
+
+class Footer extends React.Component {
+   render() {
+      return (
+         <div>
+           <footer>
+           copyright © 2017 - 2018 jesse rubin?
+           </footer>
+         </div>
+      );
+   }
+}
+
+
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +43,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
