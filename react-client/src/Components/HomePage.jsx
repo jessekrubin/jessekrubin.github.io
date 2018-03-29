36e2c38662d27fc1c6d13a508e6781618c708ed9
jessekrubin <jessekrubin@gmail.com>
2018-03-29 14:48:23 -0400

    trying to react

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component {
+  render() {
+    return (
+      <p>
+        Home page
+      </p>
+    )
+  }
+}
+
+export default HomePage;
